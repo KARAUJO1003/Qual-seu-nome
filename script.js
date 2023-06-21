b55e7f811b28e223d0edfce60fcc6ded7e3e03ff
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2023-06-21 19:01:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function pegar(){
-
-    var nome = document.getElementById("#input");
-    
-    alert(nome)
-}
+function pegar() {
+    var nome = document.getElementById("input").value.trim().toLowerCase();
+  
+    if (nome === "sara" || nome === "sara maria") {
+      alert("Correto, "+ nome.trim().toUpperCase() +  " Você é o meu amor!🥰😍");
+    } else {
+      alert("Resposta incorreta");
+    }
+  }
+  
