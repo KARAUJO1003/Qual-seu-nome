7d7298200cf762579d1216d86476f3f717fad49e
KARAUJO1003 <129511975+KARAUJO1003@users.noreply.github.com>
2023-06-22 13:15:53 -0300

    Comitando

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function pegar(){
 
-    var nome = document.getElementById("#input");
-    
-    alert(nome)
+    var nome = document.getElementById("input").value;
+
+
+    if (nome == "Sara Maria" , "sara maria" , "Sara" , "SARA" , "sara" ) {
+        alert(nome + " Seu nome é lindo")  
+
+    }
+    else {
+        alert("Resposta incorreta, Seu nome é feio")
+    }
+
+
+    document.getElementById("input").value = " ";
 }
