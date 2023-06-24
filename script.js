c7a099ea7922b6e25164369c35454770cea18852
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2023-06-24 09:00:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function pegar(){
 
     let nome = document.getElementById("input").value;
 
-    let display = document.getElementById('tela').innerHTML = "Olá " + nome + "!";
+    let display = document.getElementById('tela').innerHTML = "Olá " + nome + "! Obrigado pela sua resposta😉";
 
     
 }
